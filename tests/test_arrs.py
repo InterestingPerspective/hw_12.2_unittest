10f1b7d1c00e3dbe22e93b4d75e4e97c04b9a9c6
Lev Polevshchikov <upmindset.28@gmail.com>
2023-06-10 18:01:14 +0300

    add 3 tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,5 +12,5 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
         self.assertEqual(arrs.my_slice([], 3, 6), [])
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3, -1), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -5, -2), [1, 2])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 5], -3, -1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 5], -5, -2), [1, 2])
